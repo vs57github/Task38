6ae74a064c794e8b14dd34ec49a9d2308195e0b8
vs57github <123260473+vs57github@users.noreply.github.com>
2023-02-10 15:07:01 +0300

    Add files via upload

---
+++ Task38.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным 
+элементов массива.
+[3 7 22 2 78] -> 76 */
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите число: ");  
+    array[i] = int.Parse(Console.ReadLine());  
+}
+int max = array[0];
+int min = array[0];
+for (int i = 0; i < array.Length; i++) 
+{ 
+    if (array[i] > max) 
+    { 
+        max = array[i]; 
+    } 
+
+    if (array[i] < min) 
+    { 
+        min = array[i]; 
+    } 
+}  
+int difference = max - min;
+Console.WriteLine(difference);
